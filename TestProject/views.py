ca53cf3662ce6237fc0e146a5ff9e83862c65276
krishna <saiallanki89@gmail.com>
2021-07-27 16:18:48 +0530

    project updated

--- TestProject/views.py
+++ TestProject/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 
 def hello(request,name):
-    return render(request,"hello.html",{"name":name})
+    return render(request,"home.html",{"name":name})
     #return HttpResponse("<b>Hello "+name+"</b>")
